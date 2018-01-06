0bb4355b84e4e8acd7299fe53401e0400a7dd524
Rafał Szmigiel <rafzz@interia.eu>
2018-01-06 18:21:58 +0100

    Add files via upload

--- R/src/boot.java
+++ R/src/boot.java
@@ -10,16 +10,16 @@ public class boot {
 	
 	//private static int tab[] =  {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};//new int[20];
 	//private static int tab[] =  {14,15,16,17,18,5,6,7,8,9,10,11,19,2,0,1,3,4,12,13};
-	private static int tab[] =  new int[1000];
+	private static int tab[] =  new int[2000];
 	
-	private static int R = 6;	
+	private static int R = 5;	
 
 	private static ArrayList<String> resultList = new ArrayList<>();
 	public static void main(String[] args) {
 		
 		Random rand = new Random();
-		for(int o=0;o<1000;o++) {
-			tab[o]=rand.nextInt(1000);
+		for(int o=0;o<2000;o++) {
+			tab[o]=rand.nextInt(10000);
 		}
 		
 		for(int inn : tab) {
@@ -29,9 +29,10 @@ public class boot {
 		
 		
 		int licznik=0;
-		start();
+		//start();
 		int z=0;
 		Arrays.sort(tab);
+		start();
 		for(int i=0;i<tab.length;i++) {
 			z=i+1;
 			if(z>=tab.length) {
@@ -56,35 +57,40 @@ public class boot {
 		
 		System.out.println("\n\n---------------------KUBE£KI------------------------------");
 
-		HashMap<Double, ArrayList<Integer>> map = new HashMap<Double, ArrayList<Integer>>();
+		HashMap<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
+		//start();
+		for(int i : tab) {
+			map.put((int)Math.floor(i/R), new ArrayList<>());
+		}
+		for(int i : tab) {
+			map.get((int)Math.floor(i/R)).add(i);
+		}
 		start();
-		for(int i : tab) {
-			map.put(Math.floor(i/R), new ArrayList<>());
-		}
-		for(int i : tab) {
-			map.get(Math.floor(i/R)).add(i);
-		}
-		
 		//System.out.println(map);
 		
 		int index=0;
 		licznik=0;
-		while(index<=Collections.max(map.keySet())) {
-			if(map.get(Double.valueOf(index))!=null) {
-				for(int i=0;i<map.get(Double.valueOf(index)).size();i++) {
+		
+		double maxFromMap = Collections.max(map.keySet());
+		while(index<=maxFromMap) {
+			ArrayList<Integer> currentList =map.get(index);
+			ArrayList<Integer> nextList =map.get(index+1);
+			if(currentList!=null) {
+				for(int i=0;i<currentList.size();i++) {
 					//System.out.println();
-					for(int j=i+1;j<map.get(Double.valueOf(index)).size();j++) {
+					Integer currentPoint = currentList.get(i);
+					for(int j=i+1;j<currentList.size();j++) {
 						licznik++;
 						//System.out.print("["+map.get(Double.valueOf(index)).get(i)+", "+map.get(Double.valueOf(index)).get(j)+"]; ");
-						resultList.add("["+map.get(Double.valueOf(index)).get(i)+", "+map.get(Double.valueOf(index)).get(j)+"]; ");
+						resultList.add("["+currentPoint+", "+currentList.get(j)+"]; ");
 					}
-					if(map.get(Double.valueOf(index+1))!=null) {	
-						for(int p=0;p<map.get(Double.valueOf(index+1)).size();p++) {
+					if(nextList!=null) {	
+						for(int p=0;p<nextList.size();p++) {
 
-							if(Math.abs(map.get(Double.valueOf(index)).get(i)-map.get(Double.valueOf(index+1)).get(p))<R){
+							if(Math.abs(currentPoint-nextList.get(p))<R){
 								licznik++;
 								//System.out.print("["+map.get(Double.valueOf(index)).get(i)+", "+map.get(Double.valueOf(index+1)).get(p)+"]; ");
-								resultList.add("["+map.get(Double.valueOf(index)).get(i)+", "+map.get(Double.valueOf(index+1)).get(p)+"]; ");
+								resultList.add("["+currentPoint+", "+nextList.get(p)+"]; ");
 							}
 						}	
 					}	
